73fc8028bc5ae74234ec05d6ebbc51f23fbabfad
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2024-09-11 22:19:33 +0200

    Insert shared metadata
    Since the scroll to top component need use client, also the layout does need this. This hinders us using metadata directly within the layout. So we need to add shared metadata over the export to the single pages.

--- src/app/imprint/page.tsx
+++ src/app/imprint/page.tsx
@@ -1,10 +1,12 @@
 import Imprint from "@/components/Imprint";
+import sharedMeta from "../sharedMeta";
 
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Imprint | CocktailBerry",
   description: "CocktailBerry: Imprint page",
+  ...sharedMeta,
 };
 
 const ImprintPage = () => {
---
+++ src/app/sharedMeta.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+
+const sharedMeta: Metadata = {
+  metadataBase: new URL("https://cocktailberry.org"),
+  authors: { name: "Andre Wohnsland" },
+  creator: "CocktailBerry Team",
+  applicationName: "CocktailBerry",
+  keywords: [
+    "cocktail",
+    "cocktailberry",
+    "raspberry pi",
+    "cocktail machine",
+    "cocktail maker",
+    "python",
+    "dyi",
+    "open source",
+  ],
+};
+
+export default sharedMeta;
