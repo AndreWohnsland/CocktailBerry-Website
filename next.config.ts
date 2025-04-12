2b0cb631d726c8b853b96ae841bc185f3a4bd0c6
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2025-04-12 10:13:23 +0200

    add sharp to webpack external
    This should fix the issue that sharp is not found at the installation at vercel

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
@@ -20,6 +21,16 @@ const nextConfig = {
       },
     ],
   },
+  // add sharp to externals
+  webpack: (config) => ({
+    ...config,
+    externals: [
+      ...config.externals,
+      {
+        sharp: "commonjs sharp",
+      },
+    ],
+  }),
 };
 
 module.exports = nextConfig;
