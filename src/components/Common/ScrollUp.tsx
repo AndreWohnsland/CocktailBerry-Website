4c040317e9429a84ded2db834a26f235cd448bb8
Andre Wohnsland <50302161+AndreWohnsland@users.noreply.github.com>
2026-07-12 17:42:17 +0200

    test fix for win issue

--- src/components/Common/ScrollUp.tsx
+++ src/components/Common/ScrollUp.tsx
@@ -3,7 +3,12 @@
 import { useEffect } from "react";
 
 export default function ScrollUp() {
-  useEffect(() => window.document.scrollingElement?.scrollTo(0, 0), []);
+  // Braced body on purpose: an implicit return would hand scrollTo's return
+  // value to React as the effect cleanup, crashing on unmount in browsers
+  // where scrollTo is monkey-patched (extensions, AV injection).
+  useEffect(() => {
+    window.document.scrollingElement?.scrollTo(0, 0);
+  }, []);
 
   return null;
 }
